8fa57fffc52f12d1f1df4105def3f0d88a34d335
iidarklol <77841200+iidarklol@users.noreply.github.com>
2021-12-10 12:09:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,44 @@
-noseX = 0;
-noseY = 0;
-lwx   = 0;
-lwy   = 0;
-rwx   = 0;
-rwy   = 0;
-
-function preload(){
-
-}
-
+Lwx = 0;
+rwx = 0;
+difference = 0;
 
 function setup(){
-    canvas = createCanvas(600,400)
-    canvas.center()
-    video = createCapture(VIDEO)
-    video.size(300, 300);
-    PoseNet = ml5.poseNet(video, modelLoaded)
-    PoseNet.on("pose", gotPose)
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(10,50);
+
+    canvas = createCanvas(800,400);
+    canvas.position(430,130);
+
+    poseNet = ml5.poseNet(video,modelDone);
+    poseNet.on('pose',gotposes);
 }
-function modelLoaded(){
-    console.log("Model Loaded!");
-}
+
 function draw(){
-
+    background("#5196e3");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#00ff0a");
+    text('Divyanshu',50,400);
 }
 
-function gotPose(results){
-if(results.length > 0){
-    console.log(results);
-    noseX = results[0].pose.nose.x
-    noseY = results[0].pose.nose.y
-    lwx = results[0].pose.leftWrist.x
-    lwy = results[0].pose.leftWrist.y
-    rwx = results[0].pose.rightWrist.x
-    rwy = results[0].pose.rightWrist.y
-}else{
-    
+function modelDone(){
+    console.log("PoseNet Is Initialized And Loaded");
 }
+
+function gotposes(results,error){
+    if(error){
+        console.error(error);
+    }
+    if(results.length > 0){
+        console.log(results);
+
+        Lwx = results[0].pose.leftWrist.x;
+        rwx = results[0].pose.rightWrist.x;
+
+        difference = floor(Lwx - rwx);
+
+        console.log("rwx = "+results[0].pose.rightWrist.x + " rightWrist_y = "+results[0].pose.rightWrist.y);
+        console.log("Lwx = "+results[0].pose.leftWrist.x + " leftWrist_y = "+results[0].pose.leftWrist.y);
+    }
 }
